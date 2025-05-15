496613c8473dc296fcdbbbe28ce19df208874a3e
atom501 <quietfox8942@gmail.com>
2025-05-15 20:49:22 +0500

    adding checkerboard class

--- include/texture.h
+++ include/texture.h
@@ -21,3 +21,26 @@ public:
 
   glm::vec3 col_at_uv(const glm::vec2& uv) const override { return albedo; }
 };
+
+class Checkerboard : public Texture {
+private:
+  uint32_t width;
+  uint32_t height;
+  glm::vec3 col_a;
+  glm::vec3 col_b;
+
+public:
+  Checkerboard(uint32_t width, uint32_t height, const glm::vec3& col_a, const glm::vec3& col_b)
+      : width(width), height(height), col_a(col_a), col_b(col_b) {}
+  ~Checkerboard() = default;
+
+  glm::vec3 col_at_uv(const glm::vec2& uv) const override {
+    uint32_t u_board = std::floor(uv[0] * width);
+    uint32_t v_board = std::floor(uv[1] * height);
+
+    if ((u_board + v_board) % 2 == 0)
+      return col_a;
+    else
+      return col_b;
+  }
+};
