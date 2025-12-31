2ea3ea72e9ce983192ebf503c0abd28b5c1a801d
atom501 <quietfox8942@gmail.com>
2025-12-31 12:05:12 +0500

    use span for temp storage in stable_partition

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -325,9 +325,7 @@ Split sweep_best_span_split(uint8_t axis, std::span<size_t> prim_sorted,
 }
 
 void stable_partition(uint8_t fixed_axis, const std::vector<uint8_t>& is_left_mask,
-                      std::array<std::span<size_t>, 3> prim_axis_sort, const size_t first_right) {
-  std::vector<size_t> temp(prim_axis_sort[0].size());
-
+                      std::array<std::span<size_t>, 4> prim_axis_sort, const size_t first_right) {
   for (size_t axis = 0; axis < 3; axis++) {
     if (axis == fixed_axis) continue;
 
@@ -336,21 +334,21 @@ void stable_partition(uint8_t fixed_axis, const std::vector<uint8_t>& is_left_ma
 
     for (size_t i = 0; i < prim_axis_sort[axis].size(); i++) {
       if (is_left_mask[prim_axis_sort[axis][i]]) {
-        temp[left_count] = prim_axis_sort[axis][i];
+        prim_axis_sort[3][left_count] = prim_axis_sort[axis][i];
         left_count++;
       } else {
-        temp[first_right + right_count] = prim_axis_sort[axis][i];
+        prim_axis_sort[3][first_right + right_count] = prim_axis_sort[axis][i];
         right_count++;
       }
     }
 
-    std::copy(temp.begin(), temp.end(), prim_axis_sort[axis].begin());
+    std::copy(prim_axis_sort[3].begin(), prim_axis_sort[3].end(), prim_axis_sort[axis].begin());
   }
 }
 
 static void build_sweep_recursive(BVH& bvh, size_t node_index, size_t bb_index,
                                   std::atomic<size_t>& node_count, const std::vector<AABB>& bboxes,
-                                  std::array<std::span<size_t>, 3> prim_axis_sort,
+                                  std::array<std::span<size_t>, 4> prim_axis_sort,
                                   uint32_t& max_depth, const uint32_t max_node_prims,
                                   std::vector<uint8_t>& is_left_mask) {
   auto& curr_node = bvh.nodes[node_index];
@@ -410,14 +408,15 @@ static void build_sweep_recursive(BVH& bvh, size_t node_index, size_t bb_index,
 
   stable_partition(best_split.axis, is_left_mask, prim_axis_sort, best_split.bin_split);
 
-  std::array<std::span<size_t>, 3> left_prim_axis_sort = {
+  std::array<std::span<size_t>, 4> left_prim_axis_sort = {
       prim_axis_sort[0].first(best_split.bin_split), prim_axis_sort[1].first(best_split.bin_split),
-      prim_axis_sort[2].first(best_split.bin_split)};
+      prim_axis_sort[2].first(best_split.bin_split), prim_axis_sort[3].first(best_split.bin_split)};
 
-  std::array<std::span<size_t>, 3> right_prim_axis_sort
+  std::array<std::span<size_t>, 4> right_prim_axis_sort
       = {prim_axis_sort[0].last(prim_axis_sort[0].size() - best_split.bin_split),
          prim_axis_sort[1].last(prim_axis_sort[1].size() - best_split.bin_split),
-         prim_axis_sort[2].last(prim_axis_sort[2].size() - best_split.bin_split)};
+         prim_axis_sort[2].last(prim_axis_sort[2].size() - best_split.bin_split),
+         prim_axis_sort[3].last(prim_axis_sort[2].size() - best_split.bin_split)};
 
   // set children and recurse. set make thread for left if greater than 1024
   size_t first_child = node_count.fetch_add(2);
@@ -470,13 +469,15 @@ BVH BVH::build_sweep_bvh(const std::vector<AABB>& bboxes, const std::vector<glm:
   if (obj_count > 0) {
     bvh.obj_indices.assign(prim_indices.begin(), prim_indices.end());
 
-    // sort primitve along each axis. order will be preserved during recursion
-    std::array<std::vector<size_t>, 3> prim_axis_sort;
+    // sort primitve along each axis. order will be preserved during recursion.
+    // 4th is used as temp storage for partition
+    std::array<std::vector<size_t>, 4> prim_axis_sort;
     for (size_t axis = 0; axis < 3; axis++) {
       prim_axis_sort[axis].assign(prim_indices.begin(), prim_indices.end());
       std::sort(prim_axis_sort[axis].begin(), prim_axis_sort[axis].end(),
                 [&](size_t i, size_t j) { return centers[i][axis] < centers[j][axis]; });
     }
+    prim_axis_sort[3].assign(prim_indices.begin(), prim_indices.end());
 
     // resize to maximum nodes possible
     bvh.nodes.resize(2 * obj_count - 1);
@@ -488,8 +489,9 @@ BVH BVH::build_sweep_bvh(const std::vector<AABB>& bboxes, const std::vector<glm:
     std::atomic<size_t> node_count = 1;
     uint32_t max_depth = 0;
 
-    std::array<std::span<size_t>, 3> sorted_span = {
-        std::span(prim_axis_sort[0]), std::span(prim_axis_sort[1]), std::span(prim_axis_sort[2])};
+    std::array<std::span<size_t>, 4> sorted_span
+        = {std::span(prim_axis_sort[0]), std::span(prim_axis_sort[1]), std::span(prim_axis_sort[2]),
+           std::span(prim_axis_sort[3])};
 
     // std::vector<uint8_t> used instead of std::vector<bool> does not need locks for shared writes
     std::vector<uint8_t> is_left_mask(bboxes.size(), 0);
