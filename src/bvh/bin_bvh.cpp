6a65b476833aeb3d3710f1b537c4fe4a19305bca
atom501 <quietfox8942@gmail.com>
2026-01-04 21:58:53 +0500

    separated BVHs into different cpp files

---
+++ src/bvh/bin_bvh.cpp
@@ -0,0 +1,207 @@
+#include <bvh.h>
+
+#include <algorithm>
+#include <numeric>
+#include <thread>
+
+static size_t bin_index(uint8_t axis, const AABB& bbox, const glm::vec3& center,
+                        const size_t num_bins) {
+  int index = (center[axis] - bbox.bboxes[0][axis])
+              * (num_bins / (bbox.bboxes[1][axis] - bbox.bboxes[0][axis]));
+  return std::min(num_bins - 1, static_cast<size_t>(std::max(0, index)));
+}
+
+// get best split on all axis
+Split get_best_split(const BVHNode& node, const std::vector<size_t>& obj_indices,
+                     const std::vector<AABB>& bboxes, const std::vector<glm::vec3>& centers,
+                     const size_t num_bins, const AABB& aabb) {
+  Split best_split = {std::numeric_limits<size_t>::max(), std::numeric_limits<float>::max(), 0};
+
+  // temp bin for initialisation
+  Bin init_bin(0, AABB(glm::vec3(+std::numeric_limits<float>::max()),
+                       glm::vec3(-std::numeric_limits<float>::max())));
+
+  // check splitting for all axis
+  for (size_t axis = 0; axis < 3; axis++) {
+    std::vector<Bin> bins(num_bins, init_bin);
+
+    // filling bins
+    for (size_t i = 0; i < node.obj_count; i++) {
+      auto obj_index = obj_indices[node.first_index + i];
+      auto& bin = bins[bin_index(axis, aabb, centers[obj_index], num_bins)];
+      bin.aabb.extend(bboxes[obj_index]);
+      bin.obj_count++;
+    }
+
+    // save costs for right side
+    std::vector<float> right_cost(num_bins, 0.0f);
+    Bin extend_from_right(0, AABB(glm::vec3(+std::numeric_limits<float>::max()),
+                                  glm::vec3(-std::numeric_limits<float>::max())));
+
+    // start extending from the right to left. saving costs in an array
+    for (size_t i = num_bins - 1; i > 0; --i) {
+      extend_from_right.aabb.extend(bins[i].aabb);
+      extend_from_right.obj_count += bins[i].obj_count;
+      // cost of an empty bin is -NaN
+      right_cost[i] = extend_from_right.cost();
+    }
+
+    Bin extend_from_left(0, AABB(glm::vec3(+std::numeric_limits<float>::max()),
+                                 glm::vec3(-std::numeric_limits<float>::max())));
+
+    for (size_t i = 0; i < num_bins - 1; ++i) {
+      extend_from_left.aabb.extend(bins[i].aabb);
+      extend_from_left.obj_count += bins[i].obj_count;
+      float cost = extend_from_left.cost() + right_cost[i + 1];
+      /*
+       * This test is defined such that NaNs are automatically ignored.
+       * Thus, only valid combinations with non-empty bins are considered.
+       * IEEE-754 standard guarantees that a comparison which involves a NaN value evaluates to
+       * false
+       */
+      if (cost < best_split.cost) {
+        best_split.cost = cost;
+        best_split.bin_split = i + 1;
+        best_split.axis = axis;
+      }
+    }
+  }
+
+  return best_split;
+}
+
+// helper bvh constructor
+static void build_recursive(BVH& bvh, size_t node_index, size_t bb_index,
+                            std::atomic<size_t>& node_count, const std::vector<AABB>& bboxes,
+                            const std::vector<glm::vec3>& centers, const size_t num_bins,
+                            uint32_t& max_depth) {
+  auto& curr_node = bvh.nodes[node_index];
+  AABB curr_node_aabb = bboxes[bvh.obj_indices[curr_node.first_index]];
+  // set the AABB of current node
+  for (size_t i = 1; i < curr_node.obj_count; ++i)
+    curr_node_aabb.extend(bboxes[bvh.obj_indices[curr_node.first_index + i]]);
+
+  bvh.BB_mins_maxes[bb_index][0] = curr_node_aabb.bboxes[0].x;
+  bvh.BB_mins_maxes[bb_index][1] = curr_node_aabb.bboxes[0].y;
+  bvh.BB_mins_maxes[bb_index][2] = curr_node_aabb.bboxes[0].z;
+
+  bvh.BB_mins_maxes[bb_index + 2][0] = curr_node_aabb.bboxes[1].x;
+  bvh.BB_mins_maxes[bb_index + 2][1] = curr_node_aabb.bboxes[1].y;
+  bvh.BB_mins_maxes[bb_index + 2][2] = curr_node_aabb.bboxes[1].z;
+
+  Split best_split
+      = get_best_split(curr_node, bvh.obj_indices, bboxes, centers, num_bins, curr_node_aabb);
+
+  // use the best_split
+
+  float curr_leaf_cost
+      = curr_node_aabb.half_SA() * (curr_node.obj_count - 1);  // TODO change 1 to be configurable
+
+  size_t first_right;  // Index of the first primitive in the right child
+
+  // condition for not splitting
+  if ((best_split.bin_split == std::numeric_limits<float>::max())
+      || best_split.cost >= curr_leaf_cost) {
+    if (curr_node.obj_count > 8) {  // TODO change max prim to be configurable
+      // use the median split if many primitives
+      uint8_t axis = curr_node_aabb.largest_axis();
+
+      std::sort(bvh.obj_indices.begin() + curr_node.first_index,
+                bvh.obj_indices.begin() + curr_node.first_index + curr_node.obj_count,
+                [&](size_t i, size_t j) { return centers[i][axis] < centers[j][axis]; });
+
+      first_right = curr_node.first_index + curr_node.obj_count / 2;
+    } else {
+      // Terminate with a leaf
+      max_depth = 1;
+      return;
+    }
+  } else {
+    /* The split was good, we need to partition the primitives. subtracted with
+     * bvh.obj_indices.begin() to turn it into index and not get an iterator
+     */
+    first_right
+        = std::partition(bvh.obj_indices.begin() + curr_node.first_index,
+                         bvh.obj_indices.begin() + curr_node.first_index + curr_node.obj_count,
+                         [&](size_t i) {
+                           return bin_index(best_split.axis, curr_node_aabb, centers[i], num_bins)
+                                  < best_split.bin_split;
+                         })
+          - bvh.obj_indices.begin();
+  }
+
+  size_t first_child = node_count.fetch_add(2);
+  auto& left = bvh.nodes[first_child];
+  auto& right = bvh.nodes[first_child + 1];
+
+  left.obj_count = first_right - curr_node.first_index;
+  right.obj_count = curr_node.obj_count - left.obj_count;
+
+  left.first_index = curr_node.first_index;
+  right.first_index = first_right;
+
+  curr_node.first_index = first_child;
+  curr_node.obj_count = 0;
+
+  // run left in another thread if greater than 1024 primitives
+  std::thread left_thread;
+  uint32_t left_depth = 0;
+  uint32_t right_depth = 0;
+
+  if (left.obj_count > 1024) {
+    left_thread = std::thread(build_recursive, std::ref(bvh), first_child, first_child * 2 + 2,
+                              std::ref(node_count), std::ref(bboxes), std::ref(centers), num_bins,
+                              std::ref(left_depth));
+  } else {
+    build_recursive(bvh, first_child, first_child * 2 + 2, node_count, bboxes, centers, num_bins,
+                    left_depth);
+  }
+
+  build_recursive(bvh, first_child + 1, first_child * 2 + 3, node_count, bboxes, centers, num_bins,
+                  right_depth);
+
+  if (left_thread.joinable()) {
+    left_thread.join();
+  }
+
+  if (left_depth > right_depth) {
+    max_depth = 1 + left_depth;
+  } else {
+    max_depth = 1 + right_depth;
+  }
+}
+
+// Build and return BVH
+BVH BVH::build_bin_bvh(const std::vector<AABB>& bboxes, const std::vector<glm::vec3>& centers,
+                       const size_t num_bins) {
+  BVH bvh;
+  size_t obj_count = centers.size();
+
+  if (obj_count > 0) {
+    // initialise indices pointing to original object list
+    bvh.obj_indices.resize(obj_count);
+    std::iota(bvh.obj_indices.begin(), bvh.obj_indices.end(), 0);
+
+    // resize to maximum nodes possible
+    bvh.nodes.resize(2 * obj_count - 1);
+    bvh.BB_mins_maxes.resize((2 * obj_count - 1) * 2 + 3);
+
+    bvh.nodes[0].obj_count = obj_count;
+    bvh.nodes[0].first_index = 0;
+
+    std::atomic<size_t> node_count = 1;
+    uint32_t max_depth = 0;
+
+    // build the bvh top down
+    build_recursive(bvh, 0, 0, node_count, bboxes, centers, num_bins, max_depth);
+
+    bvh.nodes.resize(node_count);
+
+    // added padding at the end as simd loads 8 values
+    bvh.BB_mins_maxes.resize(node_count * 2 + 3);
+
+    bvh.max_depth = max_depth;
+  }
+
+  return bvh;
+}
