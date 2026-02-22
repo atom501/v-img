57403772a8f0e9b266d2bf893f328b2acf919710
atom501 <quietfox8942@gmail.com>
2026-02-22 21:33:33 +0500

    moved functions that are common to all textures

--- include/texture/texture_RGB.h
+++ include/texture/texture_RGB.h
@@ -2,6 +2,7 @@
 
 #include <hit_utils.h>
 #include <ray.h>
+#include <texture/texture_common.h>
 
 #include <algorithm>
 #include <filesystem>
@@ -15,9 +16,6 @@
 
 // None means loaded as r,g,b with each channel 0 to 255. transform according to need
 enum class TextureType { None, Image, Normals };
-
-// effects the handling of texture UVs when out of [0, 1] range
-enum class TextureWrappingMode { ClampToEdge, MirroredRepeat, Repeat };
 
 // fmt formater for TextureType
 inline auto format_as(TextureType t) {
@@ -150,37 +148,3 @@ private:
 };
 
 ImageTexture load_imagetexture(const std::filesystem::path& ImageTexture_file);
-TextureWrappingMode gltf_wrap_convert(const fastgltf::Wrap gltf_wrap);
-
-inline float handle_wrapping(float coord, TextureWrappingMode mode) {
-  switch (mode) {
-    case TextureWrappingMode::ClampToEdge:
-      return std::clamp(coord, 0.f, 1.f);
-
-    case TextureWrappingMode::Repeat: {
-      float fraction = coord - static_cast<int>(coord);
-
-      if (std::signbit(fraction))
-        return 1.f + fraction;
-      else
-        return fraction;
-    }
-
-    case TextureWrappingMode::MirroredRepeat: {
-      int int_part = static_cast<int>(coord);
-      float fraction = coord - int_part;
-
-      if (std::signbit(fraction)) {
-        if (int_part % 2)
-          return std::fabs(fraction);
-        else
-          return 1.f + fraction;
-
-      } else
-        return fraction;
-    }
-
-    default:
-      return std::clamp(coord, 0.f, 1.f);
-  }
-}
